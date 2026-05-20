d2a5ac91833a9e4dc65cb2fef44bede9280380d2
edugrin2016-collab <edugrin2016@gmail.com>
2026-05-20 18:52:25 -0300

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './presentacion 4.0.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
